d09801e1f6663d7856de13fd7020758058c903c5
Yannick Schall <yannick@handydandies.com>
2012-02-08 15:19:20 +0000

    added stylesheet_link helper with css versionning

--- _plugins/stylesheet_link.rb
+++ _plugins/stylesheet_link.rb
@@ -1,9 +1,24 @@
 module Jekyll
-  module AssetFilter
-    def asset_url(input)      
-      "/assets/themes/salsacaribecouk/css/#{input}.css?#{Time.now.to_i}"
-    end
-  end
-end
 
-Liquid::Template.register_filter(Jekyll::AssetFilter)
+    class StylesheetTag < Liquid::Tag
+      def initialize(tag_name, params, tokens)
+        super
+        @params = params
+      end
+
+      def render(context)
+        params = @params.split("|")
+        css = params[0].to_s.strip!.gsub!(/^["']/, "").gsub!(/["']$/, "")
+        media = params[1].to_s.strip!.gsub!(/^["']/, "").gsub!(/["']$/, "")
+        mtime = rand 1000000000
+        # mtime = Time.now.to_i
+        # mtime = sprintf("%.0f", mtime)
+
+        return %Q{<link rel="stylesheet" href="/assets/themes/salsacaribecouk/css/#{css}.css?#{mtime}" media="#{media}" />}
+      end
+    end
+
+  end
+
+  Liquid::Template.register_tag('stylesheet', Jekyll::StylesheetTag)
+
