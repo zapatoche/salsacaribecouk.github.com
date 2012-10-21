da77ee50fa4db021fb02bbb2b29e7c90e3d024d7
Yannick Schall <yannick@bl1nd.com>
2012-10-21 20:29:02 +0100

    added select mobile nav when width <= 480 ( iphone landacpe )

--- assets/themes/salsacaribecouk/coffee/script.js
+++ assets/themes/salsacaribecouk/coffee/script.js
@@ -1,20 +1,25 @@
 
 $(function() {
-  var aboveHeight, homeLink, homeLinkText, menuBarheight, siteNav, ul;
+  var $window, aboveHeight, homeLink, homeLinkText, links, menuBarheight, mobileNav, mobileNavOption, navBar, showing, siteNav, timer, toggleMobileNav, trigger;
   $('.site-tagline .h1').fitText(1.2, {
     minFontSize: '40px',
     maxFontSize: '70px'
   });
+  $window = $(window);
   aboveHeight = $('.site-header').outerHeight(true);
   siteNav = $('.site-nav');
   menuBarheight = siteNav.outerHeight(true);
+  navBar = siteNav.find('[role="navigation"]');
+  links = navBar.find('a');
   homeLink = siteNav.find('.home');
   homeLinkText = homeLink.text();
-  ul = siteNav.find('.navigation');
-  ul.children().each(function(i, li) {
-    return ul.prepend(li);
+  trigger = 480;
+  showing = 'standard';
+  timer = null;
+  navBar.children().each(function(i, li) {
+    return navBar.prepend(li);
   });
-  return $(window).scroll(function() {
+  $(window).scroll(function() {
     if ($(window).scrollTop() > aboveHeight) {
       siteNav.addClass('fixed').css({
         'top': '0'
@@ -24,5 +29,36 @@ $(function() {
       $('.site-nav').removeClass('fixed').next().css('padding-top', '0');
       return homeLink.text(homeLinkText);
     }
+  });
+  navBar.find('.active').click(function(event) {
+    return event.preventDefault();
+  });
+  if (navBar.length && links.length) {
+    mobileNav = $('<select></select>');
+    mobileNavOption = $('<option>-- Navigation --</option>').appendTo(mobileNav);
+    links.each(function() {
+      var link;
+      link = $(this);
+      return mobileNavOption.clone().attr('value', link.attr('href')).text(link.text()).appendTo(mobileNav);
+    });
+    mobileNav = mobileNav.wrap('<div class="mobile-nav" />').parent().delegate('select', 'change', function() {
+      return window.location = $(this).val();
+    });
+  }
+  toggleMobileNav = function() {
+    var $width;
+    $width = $window.width();
+    if (showing === 'standard' && $width <= trigger) {
+      navBar.replaceWith(mobileNav);
+      return showing = 'mobileNav';
+    } else if (showing === 'mobileNav' && $width >= trigger) {
+      mobileNav.replaceWith(navBar);
+      return showing = 'standard';
+    }
+  };
+  toggleMobileNav();
+  return $window.resize(function() {
+    if (timer) clearTimeout(timer);
+    return timer = setTimeout(toggleMobileNav, 100);
   });
 });
